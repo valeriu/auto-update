82ab0b4577a38e46836792789c7acdfb7edc46ee
Valeriu Tihai <valeriu@tihai.md>
2016-01-06 22:40:17 -0500

    MOD: Use OOP

--- auto-update.php
+++ auto-update.php
@@ -2,27 +2,97 @@
 /*
  * Plugin Name: Auto Update
  * Plugin URI: https://stylishwp.com
- * Description: Plugin to enable auto update for WordPress core, themes and plugins.
- * Text Domain: auto-update
+ * Description: This plugin enable Automatic Background Updates for WordPress core, Themes and Plugins.
+ * Text Domain: autoupdate
  * Version: 1.0.0
  * Author: Valeriu Tihai
  * Author URI: http://valeriu.tihai.ca
  * Contributors: valeriutihai
  * Donate link: https://paypal.me/valeriu/5
- * License: GPLv2
+ * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-// Enable major version updates
-add_filter( 'allow_major_auto_core_updates', '__return_true', 1 );
+defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
-// Enable minor version updates
-add_filter( 'allow_minor_auto_core_updates', '__return_true', 1 );
+// No direct access
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-//Enable Plugin updates
-add_filter( 'auto_update_plugin', '__return_true', 1 );
 
-//Enable Themes updates
-add_filter( 'auto_update_theme', '__return_true', 1 );
+/**
+ * Main Class - AutoUpdate
+ *
+ * @since 1.0.0
+ */
+class AutoUpdate {
 
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function __construct() {
+
+		// Define constants
+		add_action( 'plugins_loaded', array( &$this, 'autoupdate_filters' ), 1 );
+
+		// Load language file
+		add_action( 'plugins_loaded', array( &$this, 'autoupdate_load_textdomain' ), 2 );
+
+		// Add Setings Link
+		add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), array( &$this, 'autoupdate_links' ), 1 );
+
+	}
+
+	/**
+	 * Update function
+	 *
+	 * Update all themes, plugins and WordPress core (minor and major version)
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function autoupdate_filters() {
+		// Enable major version updates
+		add_filter( 'allow_major_auto_core_updates', '__return_true', 1 );
+
+		// Enable minor version updates
+		add_filter( 'allow_minor_auto_core_updates', '__return_true', 1 );
+
+		//Enable Plugin updates
+		add_filter( 'auto_update_plugin', '__return_true', 1 );
+
+		//Enable Themes updates
+		add_filter( 'auto_update_theme', '__return_true', 1 );
+	}
+
+	/**
+	 * Load language file
+	 *
+	 * This will load the MO file for the current locale.
+	 * The translation file must be named autoupdate-$locale.mo.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function autoupdate_load_textdomain() {
+		load_plugin_textdomain( 'autoupdate', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
+	}
+
+	/**
+	 * Add settings links to plugin page
+	 *
+	 * @since 1.0.0
+	 * @access  public
+	 */
+	public function autoupdate_links( $links ) {
+		$links[] = '<a href="https://wordpress.org/support/plugin/auto-update" target="_blank">'.__("Support", 'autoupdate').'</a>';
+		return $links;
+	}
+
+}
+// Instantiate the main class
+ new AutoUpdate();
 ?>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === Auto Update ===
 Contributors: valeriutihai
-Tags: auto update, update, plugin, theme, core
+Tags: Automatic Background Updates, Auto mode update,  Automatic updates,  Easy updates,  Latest version,  Latest WordPress version,  Latest plugin version,  Latest theme version,  Recent version,  Recent plugin versions,  Recent theme versions,  Recent WordPress version, Update core, Update plugin,  Update plugins,  Update plugins automatically,  Update theme,  Update themes,  Update themes automatically,  Update to latest version,  Update to recent version,  Update WordPress,  Update WordPress automatically,  Update WordPress minor,  Update WordPress major,  Update WordPress core,  Update wp,  Update wp automatically,  Update wp minor,  Update wp major,  Update wp core,  WordPress automatic update, Stylishwp
 Requires at least: 3.8
 Tested up to: 4.4.1
 Stable tag: 1.0.0
@@ -8,15 +8,21 @@ Donate link: https://paypal.me/valeriu/5
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Plugin to enable auto update for WordPress core, themes and plugins.
+This plugin enable Auto Update for WordPress core, Themes and Plugins.
 
 == Description ==
-Plugin to enable auto update for WordPress core, themes and plugins.
+This plugin enable Automatic Background Updates for WordPress core (minor and major versions), Themes and Plugins.
 
 == Installation ==
 1. Create the directory 'auto-update' in your '/wp-content/plugins/' directory
 2. Upload all the plugin's file to the newly created directory
 3. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+None so far.
+
+== Frequently Asked Questions ==
+No options, just activate it!
 
 == Changelog ==
 
